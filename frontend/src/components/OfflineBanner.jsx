a3eef03c299a4544a063d21bae37f550f879a0bd
Saloni Chaudhary <salonic178@gmail.com>
2026-09-05 14:48:55 +0530

    Add .gitignore and untrack node_modules

--- frontend/src/components/OfflineBanner.jsx
+++
@@ -1,8 +0,0 @@
-import { useApp } from "../context/AppContext";
-import { t } from "../lib/i18n";
-
-export default function OfflineBanner() {
-  const { online, lang } = useApp();
-  if (online) return null;
-  return <div className="offline-banner">{t("offline", lang)}</div>;
-}
